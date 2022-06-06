0082873127592154b91b78d7f0c181eadf3394ce
Rafael Sabel <rafael.sabel@hotmail.com>
2022-06-05 23:40:58 -0300

    application configs

--- hubla/config/application.rb
+++ hubla/config/application.rb
@@ -30,9 +30,12 @@ module Hubla
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join("services")
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = "pt-BR"
   end
 end
